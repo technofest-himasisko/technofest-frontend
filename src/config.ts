b6be8e4f4440218c08e55362cd5b91980029aa5b
moez-rd <slow9ie@gmail.com>
2024-07-03 14:41:00 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: process.env.BASE_URL || "https://technofest-frontend.vercel.app",
+  baseUrl: process.env.BASE_URL || "http://localhost:3000",
   appShortName: "Technofest 2024",
   appName: "Technology Festival 2024",
   appLang: "id",
