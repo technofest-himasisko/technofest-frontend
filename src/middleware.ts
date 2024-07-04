eac2a15284fae5a4170d86835722d009d5f0fb24
moez-rd <slow9ie@gmail.com>
2024-07-04 15:02:01 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,5 +11,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|images|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|images|favicon.ico|opengraph-image.png).*)",
+  ],
 };
