da798b558042965750b907d8d634c35f7a860b4d
moez-rd <slow9ie@gmail.com>
2024-07-15 00:45:36 +0700

    update

--- src/lib/actions/delete-account.ts
+++ src/lib/actions/delete-account.ts
@@ -2,9 +2,8 @@
 
 import { signOut } from "../auth/auth";
 import { userDelete } from "../fetch/v2";
-import { getServerSanctumToken } from "../utils/common";
 
 export async function deleteAccount() {
-  await userDelete((await getServerSanctumToken()) as string);
+  await userDelete();
   await signOut();
 }
